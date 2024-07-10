d27f4654ac700b66c2ceddaf13d53339152b3cfa
Rajan Yadav <rajanyadav2873@gmail.com>
2024-07-10 17:18:55 +0530

    init and setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,53 @@
+import "@/styles/globals.css";
+import "@uploadthing/react/styles.css";
+
+import { ClerkProvider } from "@clerk/nextjs";
+
+import { GeistSans } from "geist/font/sans";
+import { TopNav } from "./_components/topnav";
+import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
+import { extractRouterConfig } from "uploadthing/server";
+import { ourFileRouter } from "./api/uploadthing/core";
+import { Toaster } from "@/components/ui/sonner";
+import { CSPostHogProvider } from "./_analytics/provider";
+
+export const metadata = {
+  title: "Image Gallery",
+  description: "Upload and Download images to/from here!",
+  icons: [{ rel: "icon", url: "/favicon.ico" }],
+};
+
+export default function RootLayout({
+  children,
+  modal,
+}: {
+  children: React.ReactNode;
+  modal: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <CSPostHogProvider>
+        <html lang="en" className={`${GeistSans.variable}`}>
+          <NextSSRPlugin
+            /**
+             * The `extractRouterConfig` will extract **only** the route configs
+             * from the router to prevent additional information from being
+             * leaked to the client. The data passed to the client is the same
+             * as if you were to fetch `/api/uploadthing` directly.
+             */
+            routerConfig={extractRouterConfig(ourFileRouter)}
+          />
+          <body className={`font-sans dark`}>
+            <div className="grid h-screen grid-rows-[auto,1fr]">
+              <TopNav />
+              <main className="overflow-y-scroll p-4">{children}</main>
+              {modal}
+            </div>
+            <div id="modal-root" />
+            <Toaster />
+          </body>
+        </html>
+      </CSPostHogProvider>
+    </ClerkProvider>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,40 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import { getMyImages } from "@/server/queries";
+
+export const dynamic = "force-dynamic";
+
+async function Images() {
+  const images = await getMyImages();
+
+  return (
+    <div className="flex flex-wrap gap-4">
+      {images.map((image) => (
+        <div key={image.id} className="max-w-96">
+          <Link href={`/img/${image.id}`}>
+            <div className="flex justify-center align-middle border-solid border-red-600 ">
+              <Image src={image.url} alt={image.name} style={{ objectFit: "contain" }} width={384} height={384}/>
+            </div>
+          </Link>
+          <div>{image.name}</div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default async function HomePage() {
+  return (
+    <main className="">
+      <SignedOut>
+        <div className="h-full w-full text-center text-2xl">
+          Please Sign In
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
+    </main>
+  );
+}
